0f31819096ffa08b22e06ec32278d074d2ab42b2
Breno <bhfs.2908@gmail.com>
2021-10-05 17:34:05 -0300

    feat:add component layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import Layout from "../components/layout";
+
 export default function Home() {
-  return <div className={``}></div>;
+  return (
+    <div
+      className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}
+    >
+      <Layout titulo="Cadastro Simples">
+        <div className="p-6">Conteudo</div>
+      </Layout>
+    </div>
+  );
 }
