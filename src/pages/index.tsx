71541001e09448dacbf36d11ffa6eee374e80695
Breno <bhfs.2908@gmail.com>
2021-10-06 09:43:43 -0300

    feat:toogle pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
+import { useState } from "react";
 
 export default function Home() {
   const clientes = [
@@ -20,6 +21,12 @@ export default function Home() {
     console.log(`Excluir... ${cliente.nome}`);
   }
 
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente);
+  }
+
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+
   return (
     <div
       className={`
@@ -29,13 +36,18 @@ export default function Home() {
     `}
     >
       <Layout titulo="Cadastro Simples">
-        <div className="flex justify-end">
-          <Botao className="mb-4" cor="green">
-            Novo Cliente
-          </Botao>
-        </div>
-        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} />
-        <Formulario cliente={clientes[2]} />
+        {visivel === "tabela" ? (
+          <>
+            <div className="flex justify-end">
+              <Botao className="mb-4" cor="green" onClick={() => setVisivel("form")}>
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} />
+          </>
+        ) : (
+          <Formulario cliente={clientes[2]} cancelado={() => setVisivel("tabela")} clienteMudou={salvarCliente} />
+        )}
       </Layout>
     </div>
   );
